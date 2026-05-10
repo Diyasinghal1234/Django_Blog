19122591aca7a631b5515d01300323d9df1823bf
Diya <singhaldiya271@gmail.com>
2026-05-10 15:17:37 +0530

    added migrations

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-set -o errexit  # stop if any command fails
-
 pip install -r requirements.txt
 
-python manage.py migrate --noinput
-
 python manage.py collectstatic --noinput
+
+python manage.py migrate
